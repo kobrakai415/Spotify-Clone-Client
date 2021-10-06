ddab621673e05ac8f8ec01b3fe6bdc8df6d6e9ea
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-10-06 15:13:59 +0100

    update login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap"
 const clientId = process.env.REACT_APP_CLIENT_ID
 
 const AUTH_URL =
-  `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://strivify.vercel.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+  `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://strivify.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
 
