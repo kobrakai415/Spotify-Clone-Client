4d33bb3cea3344f2d6f54e175c577749a0a0f967
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-10-07 20:14:21 +0100

    changed to client credential flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Component } from "react"
 import Login from './components/Login.jsx';
 import Auth from "./components/Auth.js"
+import Dashboard from './pages/Dashboard';
+import { Container } from 'react-bootstrap';
 
+const clientId = process.env.REACT_APP_CLIENT_ID
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET
 
-// const client_secret = "ede976d713b14614b050b8afe5c3ac0c"
+function App() {
 
-const code = new URLSearchParams(window.location.search).get("code")
+  const [accessToken, setAccessToken] = useState(null);
 
-class App extends Component {
+  useEffect(() => {
 
-  render() {
+    handleLogin()
+    return () => {
+      localStorage.clear()
+    }
 
-    return (
-      <div className="App">
-        {code ? <Auth code={code}/> : <Login/>}
-      </div>
-    );
+  }, [])
+
+  const handleLogin = async () => {
+    try {
+      const str = clientId + ":" + clientSecret
+      const buff = Buffer.from(str, 'utf-8');
+      const base64 = buff.toString('base64');
+
+      const res = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + base64
+        },
+        body: "grant_type=client_credentials"
+      })
+
+      if (res.status === 200) {
+        const json = await res.json()
+        console.log(json)
+
+        localStorage.setItem("accessToken", json.access_token)
+        setAccessToken(json.access_token)
+      }
+      console.log(res)
+
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  return (
+    <div className="App">
+      {accessToken ? <Dashboard token={accessToken} /> : null}
+
+
+    </div>
+  );
+
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,47 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import { Container } from "react-bootstrap"
 
 const clientId = process.env.REACT_APP_CLIENT_ID
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET
 
 const AUTH_URL =
-  `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://strivify.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+  `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
 
-  useEffect(() => {
-    console.log(clientId)
-  }, [])
+  const handleLogin = async () => {
+    try {
+      const str = clientId + ":" + clientSecret
+      const buff = Buffer.from(str, 'utf-8');
+      const base64 = buff.toString('base64');
+      console.log(buff)
+      const res = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + base64
+        },
+        body: "grant_type=client_credentials"
+      })
+
+      if(res.status === 200) {
+        localStorage.setItem("accessToken", res.accessToken)
+      }
+      console.log(res)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
       style={{ minHeight: "100vh" }}
     >
-      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+      <button className="btn btn-success btn-lg" onClick={handleLogin} >
         Enter a world of Music!
-      </a>
+      </button>
     </Container>
   )
 }
